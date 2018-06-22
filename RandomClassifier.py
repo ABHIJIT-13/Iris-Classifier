bb1683953cb1abfb5be742b81a3aa4f45b7697e6
ABHIJIT-13 <jeet.txt@gmail.com>
2018-06-22 18:26:53 +0530

    Random-Classifier

---
+++ RandomClassifier.py
@@ -0,0 +1,41 @@
+import random
+
+class Scrappy():
+	"""docstring for ScrappyKNN"""
+	def fit(self, X_train,y_train):
+		self.X_train = X_train
+		self.y_train = y_train
+	
+	def predict(self, X_test):
+		predictions = []
+		for row in X_test:
+			label = random.choice(self.y_train)
+			predictions.append(label)
+
+
+		return predictions	
+
+
+
+
+
+from sklearn import datasets
+
+
+iris = datasets.load_iris()
+
+X = iris.data
+y = iris.target 
+
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size = 0.5)
+
+#from sklearn.neighbors import  KNeighborsClassifier
+my_classifier = Scrappy()
+
+my_classifier.fit(X_train,y_train)
+
+predictions = my_classifier.predict(X_test)
+
+from sklearn.metrics import accuracy_score
+print (accuracy_score(y_test, predictions))
